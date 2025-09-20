82d9fb77eff136945bcdfea57edd17f74bfc716b
vaibhav <borkarv908@gmail.com>
2025-09-20 23:53:32 +0530

    change in exam model

--- backend/models/Exam.js
+++ backend/models/Exam.js
@@ -1,30 +1,4 @@
-// import mongoose from "mongoose";
 
-// const examSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-//     description: {
-//       type: String,
-//     },
-//     createdBy: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("Exam", examSchema);
-
-
-// *****************************
-
-// backend/models/Exam.js
 import mongoose from "mongoose";
 
 const examSchema = new mongoose.Schema(
